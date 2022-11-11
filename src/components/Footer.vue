20248220f91e46c7c934e59b5bb5f44b46a045b2
Angus Hsu <42315780+Billi0ns@users.noreply.github.com>
2022-11-11 16:56:54 +0800

    misc: update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,8 +7,31 @@
     p="y-6.4vw"
     sm="py-32px"
   >
-    <div font="montserrat 500 leading-18px" text="14px white">
-      Copyright © 2022 HexSchool.All rights reserved.
+    <div
+      class="flex flex-col items-center"
+      font="montserrat 500 leading-18px"
+      text="14px white"
+      sm="flex-row"
+    >
+      <div>© 2022 All rights reserved.&nbsp;</div>
+      <div class="flex">
+        <div>Coded by&nbsp;</div>
+        <a
+          class="text-primary-1"
+          href="https://github.com/Billi0ns/2022_F2E_W1"
+          target="_blank"
+        >
+          Angus Hsu&nbsp;
+        </a>
+        <div>/ Designed by&nbsp;</div>
+        <a
+          class="text-primary-1"
+          href="https://2022.thef2e.com/users/12061549261447630282"
+          target="_blank"
+        >
+          Jenny Wu
+        </a>
+      </div>
     </div>
   </div>
 </template>
